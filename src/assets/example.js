0bf0598311f9e21f4cfc5ff563b0f856d95d02d8
Spiegel <youknow@proton.me>
2023-05-04 13:00:18 -0300

    First commit

---
+++ src/assets/example.js
@@ -0,0 +1,12 @@
+export const res = {
+    cuit: "30684490784",
+    cae: "73184057038490",
+    fechaEmision: "02/05/2023",
+    tipoComp: "6 - Factura B",
+    importeTotal: "2999.99",
+    docReceptor: "00 - CI Policía Federal",
+    docNro: "20222222223",
+    pVenta: "0143",
+    nroComp: "011180",
+    denom: "LA COLONIA SA",
+}
